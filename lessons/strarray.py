13cd90e6d523de263da6746e58b9456caf986657
William Chesser <bchesser@unc.edu>
2022-11-14 18:47:18 -0500

    Created

--- lessons/strarray.py
+++ lessons/strarray.py
@@ -12,14 +12,22 @@ class StrArray:
     def __repr__(self) -> str:  # repr should be returning valid python code.
         return f"StrArray({self.items})"
 
-    def __add__(self, rhs: str) -> StrArray:
+    def __add__(self, rhs: Union[str, StrArray]) -> StrArray:
         result: StrArray = StrArray([])
-        for item in self.items:
-            item += rhs
-            result.items.append(item)
+        if isinstance(rhs, str):
+            for item in self.items:
+                item += rhs
+                result.items.append(item)
+        else:
+            assert len(self.items) == len(rhs.items)
+            i: int = 0
+            while i < len(self.items):
+                result.items.append(self.items[i] + rhs.items[i])
+                i += 1
         return result
 
 a: StrArray = StrArray(["A", "B", "C"])
 b: StrArray = StrArray(["1", "2", "3"])
 print(a)
 print(a + "!")
+print(a + b)
